acf023b71dff64787ac7696b5795ed74d2e992ce
MardoDev <stagiaire.mardocheendolo.f2e@groupsolutioncg.com>
2024-09-13 15:55:02 +0200

    Travailler sur la page d'accueil Ajout du carousel, travail sur la section presentation et propriété
    Travail sur la section confiance et témoignage: Mise à jour du programme

---
+++ public/js/script.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 30,
+
+
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable:true,
+        dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    breakpoints:{
+        0:{
+            slidesPerView: 1
+        },
+        768:{
+            slidesPerView: 2
+        },
+        1024:{
+            slidesPerView: 3
+        },
+    }
+});
